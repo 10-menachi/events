cea8373670cc874459f624e8bbe21a615b808cb3
chrisdroid <bobitlmr188221@spu.ac.ke>
2026-07-12 21:33:13 +0300

    Configure Database Client

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,6 +1,13 @@
 import app from "./app";
 import { env } from "./config";
+import { connectDatabase } from "./lib/database";
 
-app.listen(env.port, () => {
-    console.log(`Server running on port ${env.port}`);
-});
+async function bootstrap() {
+    await connectDatabase();
+
+    app.listen(env.port, () => {
+        console.log(`Server running on port ${env.port}`);
+    });
+}
+
+bootstrap();
