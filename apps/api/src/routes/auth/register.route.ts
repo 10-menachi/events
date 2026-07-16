525d3b88a6400e29ed848d9b606540f169192f9c
chrisdroid <bobitlmr188221@spu.ac.ke>
2026-07-16 10:21:01 +0300

    Create Login Schema

--- apps/api/src/routes/auth/register.route.ts
+++ apps/api/src/routes/auth/register.route.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import registerUserController from "../../controllers/auth/register.controller";
 import validate from "../../middleware/validation.middleware";
-import registrationSchema from "../../schemas/auth/registration.schema";
+import { registrationSchema } from "../../schemas/auth/registration.schema";
 
 const router = Router();
 
